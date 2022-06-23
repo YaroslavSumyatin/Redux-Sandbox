3eaa347408534d4b66f7270cd5ba6232f920b7f4
Yaroslav Sumiatin <yaroslav_sumiatin@epam.com>
2022-06-23 19:35:55 +0300

    connected react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { bindActionCreators, createStore } from "redux";
+import * as actions from "./actions";
+import reducer from "./reducer";
+import Counter from "./counter.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>Something</div>);
+const store = createStore(reducer);
+const { dispatch } = store;
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+const update = () => {
+	const counter = (
+		<Counter
+			counter={store.getState()}
+			inc={inc}
+			dec={dec}
+			rnd={() => {
+				const randomNumber = Math.floor(Math.random() * 10 - 5);
+				rnd(randomNumber);
+			}}
+		/>
+	);
+	root.render(counter);
+};
+
+update();
+store.subscribe(update);
